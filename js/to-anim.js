8e98934d73ca13b3d885b08bd10176beebc6eb60
Nadia Pillard <ua9ond@yandex.ru>
2016-05-14 00:45:16 +0600

    animation vitesse changed

--- js/to-anim.js
+++ js/to-anim.js
@@ -2,24 +2,24 @@
 $(function () {
   var opacityElem = new TimelineMax();
     opacityElem
-      .to(".to-test__diagram", 0, {backgroundPosition: "0 0"}, '-=0.5')
-      .to("#firstShow", 1, {opacity:1})
-      .to(".to-test__diagram", 0, {backgroundPosition: "-325px 0"}, '-=1')
-      .to(".to-test__diagram", 0, {backgroundPosition: "0 -325px"},'-=0.2')
-      .to("#secondShow", 1, {opacity: 1},'=1')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -325px"},'-=1')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -0"},'=0.2')
-      .to("#thirdShow", 1, {opacity: 1},'=1')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -325px"},'-=1')
-      .to(".to-test__diagram", 0, {backgroundPosition: "0 -650px"},'=0.2')
-      .to("#forthShow", 1, {opacity: 1},'=1')
-      .to(".to-test__diagram-text", 1, {opacity: 1},'-=1')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -650px"},'-=1')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -650px"},'=0.2');
+      .to(".to-test__diagram", 0, {backgroundPosition: "0 0"}, '-=1')
+      .to("#firstShow", 0.4, {opacity:1})
+      .to(".to-test__diagram", 0, {backgroundPosition: "-325px 0"}, '-=0.4')
+      .to(".to-test__diagram", 0, {backgroundPosition: "0 -325px"})
+      .to("#secondShow", 0.4, {opacity: 1},'=0.4')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -325px"},'-=0.4')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -0"})
+      .to("#thirdShow", 0.4, {opacity: 1},'=0.4')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -325px"},'-=0.4')
+      .to(".to-test__diagram", 0, {backgroundPosition: "0 -650px"})
+      .to("#forthShow", 0.4, {opacity: 1},'=0.4')
+      .to(".to-test__diagram-text", 0.3, {opacity: 1},'-=0.4')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -650px"},'-=0.4')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -650px"});
   var controller = new ScrollMagic.Controller();
   var scene = new ScrollMagic.Scene({
     triggerElement: ".to-test",
-    offset: 300,
+    offset: 190,
     reverse: false
   })
   .setTween(opacityElem) // trigger a TweenMax.to tween
