bfe46bcc9be7dd8983e96ea2cb374a3d0422830b
Nadia Pillard <ua9ond@yandex.ru>
2016-05-14 01:03:19 +0600

    animation duration changed, header padding correxted

--- js/to-anim.js
+++ js/to-anim.js
@@ -3,23 +3,23 @@ $(function () {
   var opacityElem = new TimelineMax();
     opacityElem
       .to(".to-test__diagram", 0, {backgroundPosition: "0 0"}, '-=1')
-      .to("#firstShow", 0.4, {opacity:1})
-      .to(".to-test__diagram", 0, {backgroundPosition: "-325px 0"}, '-=0.4')
+      .to("#firstShow", 0.5, {opacity:1})
+      .to(".to-test__diagram", 0, {backgroundPosition: "-325px 0"}, '-=0.5')
       .to(".to-test__diagram", 0, {backgroundPosition: "0 -325px"})
-      .to("#secondShow", 0.4, {opacity: 1},'=0.4')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -325px"},'-=0.4')
+      .to("#secondShow", 0.5, {opacity: 1},'=0.5')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -325px"},'-=0.5')
       .to(".to-test__diagram", 0, {backgroundPosition: "-650px -0"})
-      .to("#thirdShow", 0.4, {opacity: 1},'=0.4')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -325px"},'-=0.4')
+      .to("#thirdShow", 0.5, {opacity: 1},'=0.5')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-650px -325px"},'-=0.5')
       .to(".to-test__diagram", 0, {backgroundPosition: "0 -650px"})
-      .to("#forthShow", 0.4, {opacity: 1},'=0.4')
-      .to(".to-test__diagram-text", 0.3, {opacity: 1},'-=0.4')
-      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -650px"},'-=0.4')
+      .to("#forthShow", 0.5, {opacity: 1},'=0.5')
+      .to(".to-test__diagram-text", 0.3, {opacity: 1},'-=0.5')
+      .to(".to-test__diagram", 0, {backgroundPosition: "-325px -650px"},'-=0.5')
       .to(".to-test__diagram", 0, {backgroundPosition: "-650px -650px"});
   var controller = new ScrollMagic.Controller();
   var scene = new ScrollMagic.Scene({
     triggerElement: ".to-test",
-    offset: 190,
+    offset: 100,
     reverse: false
   })
   .setTween(opacityElem) // trigger a TweenMax.to tween
